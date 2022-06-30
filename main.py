b2f740191d4545e2d8b3f91c7ec84cef044a3565
Sachin <sachnboora@gmail.com>
2022-06-30 11:23:44 +0530

    Editing a Blog

--- main.py
+++ main.py
@@ -79,9 +79,26 @@ def new_post():
     return render_template("make-post.html", form=form)
 
 
-@app.route("/edit_post", methods=["GET", "POST"])
-def edit_post():
-    pass
+@app.route("/edit_post/<post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        author=post.author,
+        img_url=post.img_url,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.author = edit_form.author.data
+        post.img_url = edit_form.img_url.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", index=post_id))
+
+    return render_template("make-post.html", form=edit_form, is_edit=True)
 
 
 if __name__ == "__main__":
